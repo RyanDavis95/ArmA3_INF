4426a78f022bd25a303c12b8ed7a15bdcbaf6226
RyanDavis95 <rwdavis@wisc.edu>
2016-04-30 11:10:26 -0500

    Update Config_Functions.hpp

--- Config_Functions.hpp
+++ Config_Functions.hpp
@@ -11,6 +11,7 @@ class INF{
                         class BloodEffects {};
                         class ActionJump {};
                         class findRespawnPoint {};
+                        class playMusic {};
             };
 };
 class MPINF {
