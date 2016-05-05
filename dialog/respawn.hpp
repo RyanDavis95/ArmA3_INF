f64deb20896e4d717278d1ddf126d8d525f29591
Ryan Davis <rwdavis@wisc.edu>
2016-05-05 16:23:28 -0500

    Update respawn.hpp

--- dialog/respawn.hpp
+++ dialog/respawn.hpp
@@ -1,1 +1,22 @@
+#define ratio 3/4
+
+
+class playerHUD {
+    idd = 2200;
+    duration = 10e10;
+    movingEnable = 0;
+    fadein = 0;
+    fadout = 0;
+    name = "respawnDialog";
+    onLoad = "uiNamespace setVariable ['respawnDialog',_this select 0]";
+    objects[] = {};
+    controls[] = {
+        INF_RscBackground_Respawn
+    };
+    class INF_RscBackground_Respawn : INF_RscBackground {
+      
+      
+    };
+};
+
 
